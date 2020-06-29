d887ab6953c842b64ed24b82626d59809dbe1758
DaRealRomz <48253436+DaRealRomz@users.noreply.github.com>
2020-06-28 23:02:28 -0400

    fix filename

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ rm -rf /tmp/forge-install
 mkdir -p /tmp/forge-install
 cd /tmp/forge-install
 wget https://files.minecraftforge.net/maven/net/minecraftforge/forge/$1/forge-$1-installer.jar
-java -jar forge-$1-installer.jar --installServer minecraft_server.$1
-mv minecraft_server*.jar $HOME/jars/
+java -jar forge-$1-installer.jar --installServer minecraft_server.$1.jar
+mv minecraft_server.$1.jar $HOME/jars/
 cd -
 rm -rf /tmp/forge-install
